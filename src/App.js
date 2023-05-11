30d7cf1c4e9e53b6a36122805b440c0aff151bf5
rodrigo <rodrigo.martinez224@gmail.com>
2023-05-11 01:58:18 -0300

    v3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
 import Navbar from "./components/Navbar";
+import Nosotros from "./components/Nosotros"
 import "./main.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <div>
-      <Navbar />
-      <ItemListContainer />
-      <ItemDetailContainer itemId={2} />
+
+      <BrowserRouter>
+
+        <Navbar />
+
+
+
+        <Routes>
+ 
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />}/>
+          <Route path="/productos" element={<ItemListContainer />}/>
+          <Route path="/productos/:categoria" element={<ItemListContainer />}/>
+          <Route path="/Nosotros" element={<Nosotros />} />
+
+        </Routes>
+        
+         
+
+      </BrowserRouter>
     </div>
   );
 }
